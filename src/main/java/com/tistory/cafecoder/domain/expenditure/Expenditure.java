ab7eba38918aaec5a0d56299bc97a964444ab696
hwk0911 <hwk0911@gmail.com>
2020-07-01 19:18:14 +0900

    지출 Entity

---
+++ src/main/java/com/tistory/cafecoder/domain/expenditure/Expenditure.java
@@ -0,0 +1,41 @@
+package com.tistory.cafecoder.domain.expenditure;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Expenditure {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long price;
+
+    @Column
+    private LocalDateTime date;
+
+    @Column
+    private String history;
+
+    @Column
+    private String location;
+
+    @Builder
+    public Expenditure(Long price, LocalDateTime date, String history, String location) {
+        this.price = price;
+        this.date = date;
+        this.history = history;
+        this.location = location;
+    }
+}
