9caaaf8ac8e81d87fb7203006c802b3c18496f76
hwk0911 <hwk0911@gmail.com>
2020-07-01 19:22:10 +0900

    일 별 수입에 대한 Entity

--- src/main/java/com/tistory/cafecoder/domain/income/Income.java
+++ src/main/java/com/tistory/cafecoder/domain/income/Income.java
@@ -1,5 +1,7 @@
 package com.tistory.cafecoder.domain.income;
 
+import com.tistory.cafecoder.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,7 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Income {
+public class Income extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,4 +25,22 @@ public class Income {
 
     @Column
     private Long price;
+
+    @Builder
+    public Income (LocalDateTime date, Long price) {
+        this.date = date;
+        this.price = price;
+    }
+
+    public Income update (LocalDateTime modifiedDate) {
+        this.date = modifiedDate;
+
+        return this;
+    }
+
+    public Income update (Long modifiedPrice) {
+        this.price = modifiedPrice;
+
+        return this;
+    }
 }
