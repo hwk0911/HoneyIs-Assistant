d25adfb15793a874ad6cbc58a2b231fa8034f577
hwk0911 <hwk0911@gmail.com>
2020-07-02 16:23:22 +0900

    Client Jpa 추가

---
+++ src/main/java/com/tistory/cafecoder/domain/product/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.tistory.cafecoder.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
