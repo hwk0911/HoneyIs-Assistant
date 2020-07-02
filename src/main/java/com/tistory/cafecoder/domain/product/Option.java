a9a52d9e1c54045496671b726faac985a95b96b7
hwk0911 <hwk0911@gmail.com>
2020-07-02 16:23:22 +0900

    Options -> Option rename

---
+++ src/main/java/com/tistory/cafecoder/domain/product/Option.java
@@ -0,0 +1,46 @@
+package com.tistory.cafecoder.domain.product;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long productId;
+
+    @Column
+    private String color;
+
+    @Column
+    private String size;
+
+    @Column
+    private Long amount;
+
+    @Builder
+    public Option(Long productId, String color, String size, Long amount) {
+        this.productId = productId;
+        this.color = color;
+        this.size = size;
+        this.amount = amount;
+    }
+
+    public Option update(Long amount) {
+        this.amount -= amount;
+
+        return this;
+    }
+}
