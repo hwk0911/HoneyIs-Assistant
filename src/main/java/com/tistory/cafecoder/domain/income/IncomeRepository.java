ad63dd057893ec87a24d16dd700b352fb779ded0
hwk0911 <hwk0911@gmail.com>
2020-07-02 16:23:22 +0900

    Income Jpa 추가

---
+++ src/main/java/com/tistory/cafecoder/domain/income/IncomeRepository.java
@@ -0,0 +1,7 @@
+package com.tistory.cafecoder.domain.income;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+}
