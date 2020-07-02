69865915c0e36b0463685fdf53e1e72c9418daa4
hwk0911 <hwk0911@gmail.com>
2020-07-02 16:23:22 +0900

    Option Jpa 추가

---
+++ src/main/java/com/tistory/cafecoder/domain/product/OptionRepository.java
@@ -0,0 +1,6 @@
+package com.tistory.cafecoder.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+}
