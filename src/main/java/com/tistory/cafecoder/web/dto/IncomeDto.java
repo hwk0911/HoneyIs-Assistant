187ee6b54d60697c6441af4c1a71454f7f4cd3db
hwk0911 <hwk0911@gmail.com>
2020-07-03 17:24:57 +0900

    Income CRUD를 위한 incomeDto 생성

---
+++ src/main/java/com/tistory/cafecoder/web/dto/IncomeDto.java
@@ -0,0 +1,32 @@
+package com.tistory.cafecoder.web.dto;
+
+import com.tistory.cafecoder.domain.income.Income;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class IncomeDto {
+    private Long id;
+    private String email;
+    private LocalDateTime date;
+    private Long price;
+    private String memo;
+
+    public IncomeDto(Income entity) {
+        this.id = entity.getId();
+        this.email = entity.getEmail();
+        this.date = entity.getDate();
+        this.price = entity.getPrice();
+        this.memo = entity.getMemo();
+    }
+
+    public Income toEntity() {
+        return new Income().builder()
+                .email(this.email)
+                .date(this.date)
+                .price(this.price)
+                .memo(this.memo)
+                .build();
+    }
+}
