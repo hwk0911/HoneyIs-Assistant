76a04692d0cc3a9ce380ef59c8a0c0a770b71737
hwk0911 <hwk0911@gmail.com>
2020-07-06 18:13:22 +0900

    한 달 기준으로 Income 검색을 위한 메소드 추가

--- src/main/java/com/tistory/cafecoder/domain/income/IncomeRepository.java
+++ src/main/java/com/tistory/cafecoder/domain/income/IncomeRepository.java
@@ -2,8 +2,10 @@ package com.tistory.cafecoder.domain.income;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
     List<Income> findByEmail(String email);
+    List<Income> findByDateBetween(LocalDateTime start, LocalDateTime end);
 }
