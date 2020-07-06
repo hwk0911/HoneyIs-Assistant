182b70dedb0ca71fad309a7607cf3187792c4e36
hwk0911 <hwk0911@gmail.com>
2020-07-06 17:20:22 +0900

    year와 month를 받도록 설정

--- src/main/java/com/tistory/cafecoder/web/IndexController.java
+++ src/main/java/com/tistory/cafecoder/web/IndexController.java
@@ -1,13 +1,28 @@
 package com.tistory.cafecoder.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-
     @GetMapping("/")
-    public String index () {
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/expenditure")
+    public String expenditure(Model model) {
+
+
+        return "expenditure";
+    }
+
+    @GetMapping("/income/{year}/{month}")
+    public String income(Model model, @PathVariable Long year, @PathVariable Long month) {
+
+
+        return "income";
     }
 }
