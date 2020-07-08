8d845f35ff6d05cbfa50b667bb05a0182cf2538b
hwk0911 <hwk0911@gmail.com>
2020-07-08 20:01:14 +0900

    ajax로 데이터 받아서 처리 현재 미완성

--- src/main/java/com/tistory/cafecoder/web/IndexController.java
+++ src/main/java/com/tistory/cafecoder/web/IndexController.java
@@ -2,14 +2,14 @@ package com.tistory.cafecoder.web;
 
 import com.tistory.cafecoder.domain.income.Income;
 import com.tistory.cafecoder.service.IncomeService;
+import com.tistory.cafecoder.web.dto.TermIncomeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -45,26 +45,19 @@ public class IndexController {
         return "income";
     }
 
-    @PostMapping("/api/v1/getMonthList")
-    public String getMonthList(Model model, @RequestParam("where") String where, @RequestParam("year") int year, @RequestParam("month") int month) {
-        List<Income> incomeList;
+    @PostMapping("/api/v1/termincome")
+    public Model getMonthList(Model model, @RequestBody TermIncomeDto termIncomeDto) {
+        model.addAttribute("start", termIncomeDto.getStart());
+        model.addAttribute("end", termIncomeDto.getEnd());
+        model.addAttribute("incomeList", this.incomeService.getMonthList(termIncomeDto.getStart(), termIncomeDto.getEnd()));
 
-        System.out.println(year + " " + month);
+        System.out.println(model.toString());
 
-        if(where.equals("prev")) {
-            year -= (month - 1) < 1 ? 1 : 0;
-            month = (month - 1) < 1 ? 12 : month - 1;
-        }
-        else {
-            year += (month + 1) > 12 ? 1 : 0;
-            month = (month + 1) > 12 ? 1 : month + 1;
-        }
-        incomeList = this.incomeService.getMonthList(year, month);
+        return model;
+    }
 
-        model.addAttribute("incomeList", incomeList);
-        model.addAttribute("year", year);
-        model.addAttribute("month", month);
-
-        return "income";
+    @GetMapping("/incomelist")
+    public String incomeList () {
+        return "incomeList";
     }
 }
