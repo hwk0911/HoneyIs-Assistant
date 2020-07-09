26ab04f7756a4891856bc8f0a33046b1e14139fa
hwk0911 <hwk0911@gmail.com>
2020-07-09 18:11:07 +0900

    Income Controller

---
+++ src/main/java/com/tistory/cafecoder/web/IncomeApiController.java
@@ -0,0 +1,20 @@
+package com.tistory.cafecoder.web;
+
+import com.tistory.cafecoder.service.IncomeService;
+import com.tistory.cafecoder.web.dto.IncomeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class IncomeApiController {
+
+    private final IncomeService incomeService;
+
+    @PostMapping("/api/v1/saveincome")
+    public Long saveIncome(@RequestBody IncomeDto incomeDto) {
+        return this.incomeService.addIncome(incomeDto);
+    }
+}
