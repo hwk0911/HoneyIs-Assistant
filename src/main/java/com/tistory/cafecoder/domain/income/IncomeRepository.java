bff2157de6374490992244ddc3d0de5bf66adc8f
hwk0911 <hwk0911@gmail.com>
2020-07-10 18:03:58 +0900

    로그인 된 사용자 기준으로 데이터 검색

--- src/main/java/com/tistory/cafecoder/domain/income/IncomeRepository.java
+++ src/main/java/com/tistory/cafecoder/domain/income/IncomeRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
     List<Income> findByEmail(String email);
-    List<Income> findByDateBetweenOrderByDateAsc(LocalDate start, LocalDate end);
+    List<Income> findByEmailAndDateBetweenOrderByDateAsc(String email, LocalDate start, LocalDate end);
 }
