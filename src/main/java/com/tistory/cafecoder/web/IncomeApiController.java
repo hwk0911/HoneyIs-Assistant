99feb8bcaedcc723597768260cc66bd650c3e8bf
hwk0911 <hwk0911@gmail.com>
2020-07-24 18:27:56 +0900

    Delete Mapping

--- src/main/java/com/tistory/cafecoder/web/IncomeApiController.java
+++ src/main/java/com/tistory/cafecoder/web/IncomeApiController.java
@@ -3,9 +3,7 @@ package com.tistory.cafecoder.web;
 import com.tistory.cafecoder.service.IncomeService;
 import com.tistory.cafecoder.web.dto.IncomeDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,13 +11,18 @@ public class IncomeApiController {
 
     private final IncomeService incomeService;
 
-    @PostMapping("/api/v1/saveincome")
+    @PostMapping("/api/v1/income/save")
     public Long saveIncome(@RequestBody IncomeDto incomeDto) {
         return this.incomeService.addIncome(incomeDto);
     }
 
-    @PostMapping("/api/v1/updateincome")
+    @PutMapping("/api/v1/income/update")
     public Long updateIncome(@RequestBody IncomeDto updateDto) {
         return this.incomeService.update(updateDto);
+    }
+
+    @DeleteMapping("/api/v1/income/delete/{id}")
+    public Long deleteIncome(@PathVariable("id") Long id) {
+        return this.incomeService.delete(id);
     }
 }
