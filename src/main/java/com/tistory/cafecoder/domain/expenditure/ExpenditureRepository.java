cc5112711f3f161536cc645a4c4211121a8993f2
hwk0911 <hwk0911@gmail.com>
2020-07-24 17:56:57 +0900

    이메일 AND 시작날짜 ~ 종료날짜 오름차순 JPA

--- src/main/java/com/tistory/cafecoder/domain/expenditure/ExpenditureRepository.java
+++ src/main/java/com/tistory/cafecoder/domain/expenditure/ExpenditureRepository.java
@@ -2,5 +2,9 @@ package com.tistory.cafecoder.domain.expenditure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> {
+    List<Expenditure> findByEmailAndDateBetweenOrderByDateAsc(String email, LocalDate start, LocalDate end);
 }
