e7c62db76eeac27749ab255aa6c9fe717115c2b0
hwk0911 <hwk0911@gmail.com>
2020-07-24 17:58:58 +0900

    쓰지않는 import 제거

--- src/main/java/com/tistory/cafecoder/web/IncomeApiController.java
+++ src/main/java/com/tistory/cafecoder/web/IncomeApiController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-
 @RequiredArgsConstructor
 @RestController
 public class IncomeApiController {
