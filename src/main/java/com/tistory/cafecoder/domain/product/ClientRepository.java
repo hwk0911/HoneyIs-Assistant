8d6c373c3bdba2c465071d4974a0e01e50a9465f
hwk0911 <hwk0911@gmail.com>
2020-07-25 20:10:29 +0900

    검색을 통한 조회를 위해 메소드 추가

--- src/main/java/com/tistory/cafecoder/domain/product/ClientRepository.java
+++ src/main/java/com/tistory/cafecoder/domain/product/ClientRepository.java
@@ -3,4 +3,5 @@ package com.tistory.cafecoder.domain.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findByName(String searchWord);
 }
