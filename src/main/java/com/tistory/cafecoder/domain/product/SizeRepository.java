e0ac31e664eb3057b982709000822452be9d2383
hwk0911 <hwk0911@gmail.com>
2020-07-29 17:07:21 +0900

    SizeRepository.java

---
+++ src/main/java/com/tistory/cafecoder/domain/product/SizeRepository.java
@@ -0,0 +1,7 @@
+package com.tistory.cafecoder.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SizeRepository extends JpaRepository<Size, Long> {
+    Size findBySize(String size);
+}
