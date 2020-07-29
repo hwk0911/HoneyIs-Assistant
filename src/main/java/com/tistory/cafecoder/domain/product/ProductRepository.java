c97a64fd983e3468f3ec962eaafd58f364bf14d3
hwk0911 <hwk0911@gmail.com>
2020-07-29 17:07:27 +0900

    ProductRepository.java

--- src/main/java/com/tistory/cafecoder/domain/product/ProductRepository.java
+++ src/main/java/com/tistory/cafecoder/domain/product/ProductRepository.java
@@ -3,5 +3,5 @@ package com.tistory.cafecoder.domain.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Product findByName(String name);
 }
