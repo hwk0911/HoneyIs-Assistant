4716010efcd1b5412ec0adca1c07fc26218e27a7
hwk0911 <hwk0911@gmail.com>
2020-07-29 16:00:31 +0900

    정규화를 통한 옵션 참조방식 엔티티로 변경

--- src/main/java/com/tistory/cafecoder/domain/product/Product.java
+++ src/main/java/com/tistory/cafecoder/domain/product/Product.java
@@ -1,5 +1,6 @@
 package com.tistory.cafecoder.domain.product;
 
+import com.tistory.cafecoder.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Product {
+public class Product extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,11 +19,29 @@ public class Product {
     private Long clientId;
 
     @Column
-    private String productName;
+    private String name;
+
+    @Column
+    private Long colorId;
+
+    @Column
+    private Long sizeId;
+
+    @Column
+    private Long amount;
 
     @Builder
-    public Product(Long clientId, String productName) {
+    public Product(Long clientId, String name, Long colorId, Long sizeId) {
+        this.amount = 0L;
+
         this.clientId = clientId;
-        this.productName = productName;
+        this.name = name;
+        this.colorId = colorId;
+        this.sizeId = sizeId;
+    }
+
+    public void update(String name, Long colorId, Long sizeId, Long amount) {
+        this.name = name;
+        this.clientId = clientId;
     }
 }
