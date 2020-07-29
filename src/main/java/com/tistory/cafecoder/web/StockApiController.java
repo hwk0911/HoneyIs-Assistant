cdb5a8dee35def8eb429586527250783004cd983
hwk0911 <hwk0911@gmail.com>
2020-07-29 17:06:53 +0900

    StockApiController.java

---
+++ src/main/java/com/tistory/cafecoder/web/StockApiController.java
@@ -0,0 +1,22 @@
+package com.tistory.cafecoder.web;
+
+import com.tistory.cafecoder.service.StockService;
+import com.tistory.cafecoder.web.dto.NewestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StockApiController {
+
+    private final StockService stockService;
+
+    @PostMapping("/api/v1/stock/newest")
+    public Long newest(@RequestBody NewestDto newestDto) {
+        System.out.println(newestDto.getName());
+
+        return this.stockService.newestSave(newestDto);
+    }
+}
