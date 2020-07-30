e7dcc2ee51fd9d0027efa954b5c86ab9ed95e7ed
hwk0911 <hwk0911@gmail.com>
2020-07-30 15:14:55 +0900

    ajax 양식 맞춰서 Dto 양식 변경

--- src/main/java/com/tistory/cafecoder/web/dto/NewestDto.java
+++ src/main/java/com/tistory/cafecoder/web/dto/NewestDto.java
@@ -5,26 +5,19 @@ import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
 @Getter
 @NoArgsConstructor
 public class NewestDto {
     private Long clientId;
     private String name;
-    private List<String> color;
-    private List<String> size;
+    private String color;
+    private String size;
 
     public NewestDto(Long clientId, String name, String color, String size) {
         this.clientId = clientId;
         this.name = name;
-
-        this.color = Arrays.asList(color.split(","));
-
-        if(size.equals("")) {
-            size = "FREE";
-        }
-
-        this.size = Arrays.asList(size.split(","));
+        this.color = color;
+        this.size = size;
     }
 }
