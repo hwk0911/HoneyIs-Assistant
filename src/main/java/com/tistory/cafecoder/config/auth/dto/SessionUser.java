8b4ef72d0297790af6e2c1a6e92e5dd5070e7fe5
hwk0911 <hwk0911@gmail.com>
2020-08-06 18:26:59 +0900

    세션에 xlsx 파일 분석내용 들어가도록 수정

--- src/main/java/com/tistory/cafecoder/config/auth/dto/SessionUser.java
+++ src/main/java/com/tistory/cafecoder/config/auth/dto/SessionUser.java
@@ -4,16 +4,32 @@ import com.tistory.cafecoder.domain.user.User;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
 
 @Getter
 public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
+    private Set<Map.Entry<String, Integer>> sendList;
+    private Set<Map.Entry<String, Integer>> orderList;
 
     public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
+    }
+
+    public SessionUser setSendList (Set<Map.Entry<String, Integer>> sendList) {
+        this.sendList = sendList;
+
+        return this;
+    }
+
+    public SessionUser setOrderList (Set<Map.Entry<String, Integer>> orderList) {
+        this.orderList = orderList;
+
+        return this;
     }
 }
