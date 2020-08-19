c967fe9921ecadc0306a7faf837f056af47e9ff2
hwk0911 <hwk0911@gmail.com>
2020-08-19 19:27:06 +0900

    검색 예외처리를 위한 새로운 검색 방식 적용

--- src/main/java/com/tistory/cafecoder/domain/product/ProductRepository.java
+++ src/main/java/com/tistory/cafecoder/domain/product/ProductRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByName(String name);
+    Product findByNameAndColorId(String name, Long colorId);
+    List<Product> findByName(String name);
     List<Product> findByClientId(Long clientId);
+    Product findByCode(String code);
 }
