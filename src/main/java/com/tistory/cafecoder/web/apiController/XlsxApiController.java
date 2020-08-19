14dfd1fb4d992612334f0c66b31577caf010ccc5
hwk0911 <hwk0911@gmail.com>
2020-08-19 20:29:19 +0900

    발주처를 기준으로 묶기 완료

--- src/main/java/com/tistory/cafecoder/web/apiController/XlsxApiController.java
+++ src/main/java/com/tistory/cafecoder/web/apiController/XlsxApiController.java
@@ -2,7 +2,9 @@ package com.tistory.cafecoder.web.apiController;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
+import com.tistory.cafecoder.domain.product.ProductRepository;
 import com.tistory.cafecoder.service.XlsxService;
+import com.tistory.cafecoder.web.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,21 +12,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class XlsxApiController {
     private final XlsxService xlsxService;
+    private final ProductRepository productRepository;
 
     @PostMapping("/api/v1/xlsx/analyze/{method}")
-    public String setOrder(@RequestParam(value = "files") List<MultipartFile> multipartFiles, @PathVariable("method") String method, @LoginUser SessionUser user) {
-        System.out.println(multipartFiles.size());
+    public Long setOrder(@RequestParam(value = "files") List<MultipartFile> multipartFiles, @PathVariable("method") String method, @LoginUser SessionUser user) {
+        Map<Long, Long> productMap = this.xlsxService.getResult(multipartFiles);
 
-        if (method.equals("order")) {
-            return user.setOrderList(this.xlsxService.getResult(multipartFiles)).getEmail();
-        } else {
-            return user.setSendList(this.xlsxService.getResult(multipartFiles)).getEmail();
-        }
+        user.setXlsxResult(this.xlsxService.groupByClient(productMap));
+
+        return 1L;
     }
 }
