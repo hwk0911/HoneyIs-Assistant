d03e93fef108afe864de2e9dccdc8c2da7d434ea
hwk0911 <hwk0911@gmail.com>
2020-08-20 19:20:33 +0900

    일부 주석처리

--- src/main/java/com/tistory/cafecoder/web/OrderController.java
+++ src/main/java/com/tistory/cafecoder/web/OrderController.java
@@ -2,7 +2,6 @@ package com.tistory.cafecoder.web;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
-import com.tistory.cafecoder.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,9 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class OrderController {
-
-    private final OrderService orderService;
-
     @GetMapping("/xlsx")
     public String xlsx(Model model, @LoginUser SessionUser user) {
         model.addAttribute("user", user.getEmail());
