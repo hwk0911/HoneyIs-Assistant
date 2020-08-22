f373161270b79b2f5fbb5b38be3764a4dc55bd26
hwk0911 <hwk0911@gmail.com>
2020-08-22 16:53:29 +0900

    세션을 통해 데이터를 전달받도록 변경

--- src/main/java/com/tistory/cafecoder/web/XlsxController.java
+++ src/main/java/com/tistory/cafecoder/web/XlsxController.java
@@ -2,20 +2,31 @@ package com.tistory.cafecoder.web;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
+import com.tistory.cafecoder.web.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @RequiredArgsConstructor
 @Controller
 public class XlsxController {
     @GetMapping("/xlsx/result/{method}")
-    public String orderList (Model model, @LoginUser SessionUser user, @PathVariable("method") String method) {
+    public String orderList (Model model, @LoginUser SessionUser user, @PathVariable("method") String method, HttpServletRequest request) {
         model.addAttribute("loginUser", user.getEmail());
+        HttpSession session = request.getSession();
 
-        model.addAttribute("orderAnalyze", user.getXlsxResult());
+        Map<String, List<ProductDto>> orderMap = (Map<String, List<ProductDto>>) session.getAttribute(user.getEmail());
+        Set<Map.Entry<String, List<ProductDto>>> orderSet = orderMap.entrySet();
+
+        model.addAttribute("orderAnalyze", orderSet);
 
         return "xlsxResult";
     }
