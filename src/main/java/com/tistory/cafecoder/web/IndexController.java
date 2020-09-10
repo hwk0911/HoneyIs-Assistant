e73f5920b863a6698b62a6937d46e49566787280
hwk0911 <hwk0911@gmail.com>
2020-09-10 23:10:51 +0900

    상품명을 기준으로 발주처 미지정상품 출력하기위한 컨트롤러

--- src/main/java/com/tistory/cafecoder/web/IndexController.java
+++ src/main/java/com/tistory/cafecoder/web/IndexController.java
@@ -2,40 +2,45 @@ package com.tistory.cafecoder.web;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
-import com.tistory.cafecoder.domain.income.Income;
-import com.tistory.cafecoder.domain.product.Client;
-import com.tistory.cafecoder.service.ClientService;
-import com.tistory.cafecoder.service.ExpenditureService;
-import com.tistory.cafecoder.service.IncomeService;
-import com.tistory.cafecoder.web.dto.IncomeDto;
+import com.tistory.cafecoder.service.StockService;
+import com.tistory.cafecoder.web.dto.ProductDto;
+
 import lombok.RequiredArgsConstructor;
+
 import org.mariadb.jdbc.internal.logging.Logger;
 import org.mariadb.jdbc.internal.logging.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import javax.mail.Session;
-import javax.servlet.http.HttpSession;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.util.HashSet;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+    private final StockService stockService;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         if (user != null) {
             model.addAttribute("loginUser", user.getEmail());
+
+            HashSet<String> undefinedStockSet = this.stockService.undefinedStock(user.getEmail());
+
+            if (undefinedStockSet != null) {
+                model.addAttribute("undefinedCount", undefinedStockSet.size());
+                model.addAttribute("productMap", undefinedStockSet);
+            }
+
             log.info("{}", user.getName());
         }
 
+
+
+
         return "index";
     }
-
-
 }
