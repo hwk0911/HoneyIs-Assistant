13e10c9458705baf8fdf568c9d9622b6fe12452d
hwk0911 <hwk0911@gmail.com>
2020-09-14 03:41:50 +0900

    xlsx 분석방식 변경으로인한 수정 및 코드 리팩토링

--- src/main/java/com/tistory/cafecoder/web/apiController/XlsxApiController.java
+++ src/main/java/com/tistory/cafecoder/web/apiController/XlsxApiController.java
@@ -2,9 +2,9 @@ package com.tistory.cafecoder.web.apiController;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
-import com.tistory.cafecoder.domain.product.ProductRepository;
 import com.tistory.cafecoder.service.XlsxService;
-import com.tistory.cafecoder.web.dto.ProductDto;
+import com.tistory.cafecoder.web.dto.AmountDto;
+import com.tistory.cafecoder.web.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +21,11 @@ import java.util.Map;
 @RestController
 public class XlsxApiController {
     private final XlsxService xlsxService;
-    private final ProductRepository productRepository;
 
     @PostMapping("/api/v1/xlsx/analyze/{method}")
     public Long setOrder(@RequestParam(value = "files") List<MultipartFile> multipartFiles, @PathVariable("method") String method, @LoginUser SessionUser user, HttpServletRequest request) {
-        Map<Long, Long> productMap = this.xlsxService.getResult(multipartFiles, user.getEmail());
-        Map<String, List<ProductDto>> groupResult = this.xlsxService.groupByClient(productMap, user.getEmail());
+        Map<Long, AmountDto> productMap = this.xlsxService.getResult(multipartFiles, user.getEmail());
+        Map<String, List<OrderDto>> groupResult = this.xlsxService.groupByClient(productMap, user.getEmail());
 
         HttpSession session = request.getSession();
         session.setAttribute(user.getEmail(), groupResult);
