44fe06308dd7836ff56c9186b6ea374cdbde5b7c
hwk0911 <hwk0911@gmail.com>
2020-09-15 03:29:14 +0900

    통합검색을 위한 컨트롤러 추가

--- src/main/java/com/tistory/cafecoder/web/IndexController.java
+++ src/main/java/com/tistory/cafecoder/web/IndexController.java
@@ -2,8 +2,10 @@ package com.tistory.cafecoder.web;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
+import com.tistory.cafecoder.service.ClientService;
 import com.tistory.cafecoder.service.StockService;
 
+import com.tistory.cafecoder.web.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
 
 import org.mariadb.jdbc.internal.logging.Logger;
@@ -14,12 +16,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
     private final StockService stockService;
+    private final ClientService clientService;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
@@ -41,4 +47,20 @@ public class IndexController {
 
         return "index";
     }
+
+    @GetMapping("/search/total")
+    public String totalSearch(@RequestParam("searchWord") String searchWord, @LoginUser SessionUser user, Model model) {
+        if (user != null) {
+            model.addAttribute("loginUser", user.getEmail());
+
+            model.addAttribute("clientList", this.clientService.search(searchWord, user.getEmail()));
+            model.addAttribute("clientResult", this.stockService.stockClientSearch(searchWord, user.getEmail()));
+            model.addAttribute("stockResult", this.stockService.stockSearch(searchWord, user.getEmail()));
+        } else {
+            return "index";
+        }
+
+        return "search";
+    }
+
 }
