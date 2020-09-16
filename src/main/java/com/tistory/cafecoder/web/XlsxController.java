b83f53b3b894e157054f27d711e4b4a44c457d77
hwk0911 <hwk0911@gmail.com>
2020-09-17 00:26:43 +0900

    공제 결과를 표시하기위한 컨트롤러 추가

--- src/main/java/com/tistory/cafecoder/web/XlsxController.java
+++ src/main/java/com/tistory/cafecoder/web/XlsxController.java
@@ -2,12 +2,12 @@ package com.tistory.cafecoder.web;
 
 import com.tistory.cafecoder.config.auth.LoginUser;
 import com.tistory.cafecoder.config.auth.dto.SessionUser;
-import com.tistory.cafecoder.web.dto.ProductDto;
+import com.tistory.cafecoder.service.StockService;
+import com.tistory.cafecoder.web.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -18,15 +18,33 @@ import java.util.Set;
 @RequiredArgsConstructor
 @Controller
 public class XlsxController {
-    @GetMapping("/xlsx/result/{method}")
-    public String orderList (Model model, @LoginUser SessionUser user, @PathVariable("method") String method, HttpServletRequest request) {
+
+    private final StockService stockService;
+
+    @GetMapping("/xlsx/result")
+    public String orderList (Model model, @LoginUser SessionUser user, HttpServletRequest request) {
         model.addAttribute("loginUser", user.getEmail());
         HttpSession session = request.getSession();
 
-        Map<String, List<ProductDto>> orderMap = (Map<String, List<ProductDto>>) session.getAttribute(user.getEmail());
-        Set<Map.Entry<String, List<ProductDto>>> orderSet = orderMap.entrySet();
+        Map<String, List<OrderDto>> orderMap = (Map<String, List<OrderDto>>) session.getAttribute(user.getEmail());
+        Set<Map.Entry<String, List<OrderDto>>> orderSet = orderMap.entrySet();
 
         model.addAttribute("orderAnalyze", orderSet);
+
+        return "xlsxResult";
+    }
+
+    @GetMapping("/xlsx/result/deduction")
+    public String deductionResult (@LoginUser SessionUser user, Model model, HttpServletRequest request) {
+        model.addAttribute("loginUser", user.getEmail());
+
+        HttpSession session = request.getSession();
+
+        Map<String, List<OrderDto>> orderMap = (Map<String, List<OrderDto>>) session.getAttribute(user.getEmail());
+        Set<Map.Entry<String, List<OrderDto>>> orderSet = this.stockService.getDeductionResult(orderMap).entrySet();
+
+        model.addAttribute("orderAnalyze", orderSet);
+        model.addAttribute("deducted", true);
 
         return "xlsxResult";
     }
